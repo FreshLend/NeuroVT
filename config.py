43bbb9fac8b5e299bcae69acf8c4816b11183cfa
FreshGame <vasin.oleg2010@mail.ru>
2026-07-13 14:52:56 +1000

    .

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-DISABLED_MODULES = ["Chatterbox", "Whisper"] # ["Chatterbox", "Silero", "Whisper", "OpenAi_Compatible", "Live2D"]
+DISABLED_MODULES = ["Silero", "Whisper"] # ["Chatterbox", "Silero", "Whisper", "OpenAi_Compatible", "Live2D"]
